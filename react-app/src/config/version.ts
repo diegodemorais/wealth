706d7042aeb45d3257bee36ca349edd17c2d7838
Diego de Morais <diegodemorais@gmail.com>
2026-04-14 22:10:29 -0300

    chore: bump version to 0.1.26 [CI build]

--- react-app/src/config/version.ts
+++ react-app/src/config/version.ts
@@ -1,3 +1,3 @@
 // Auto-generated on build
-export const DASHBOARD_VERSION = '0.1.25';
-export const BUILD_DATE = '2026-04-15T01:01:23.161Z';
+export const DASHBOARD_VERSION = '0.1.26';
+export const BUILD_DATE = '2026-04-15T01:10:20.493Z';
