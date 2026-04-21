33e46310362608a1670bcc8431d5fbc76663eaf7
Diego de Morais <diegodemorais@gmail.com>
2026-04-21 18:57:45 -0300

    chore: Increment version (post-deploy)

--- react-app/src/config/version.ts
+++ react-app/src/config/version.ts
@@ -1,3 +1,3 @@
 // Auto-generated on build
-export const DASHBOARD_VERSION = '1.0.31';
-export const BUILD_DATE = '2026-04-21T21:47:28.060Z';
+export const DASHBOARD_VERSION = '1.0.33';
+export const BUILD_DATE = '2026-04-21T21:56:49.227Z';
